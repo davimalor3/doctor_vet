e495337fb03129875da06fc2ad6a71822c96645e
Davi Nobre <davi.nbtec@gmail.com>
2025-11-07 10:57:46 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOME</title>
+</head>
+
+<body>
+    <?php include 'includes/header.php'; ?>
+    <h1>Hello</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum,
+        labore, veniam cupiditate voluptatum dolorem illo placeat qui suscipit similique
+        necessitatibus iste nostrum consectetur. Dolorum modi quaerat voluptatibus necessitatibus nobis
+        at.</p>
+
+    <?php include 'includes/header.php'; ?>
+</body>
+
+</html>

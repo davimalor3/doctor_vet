f1cad0563f53eaeb901807429878f6f63825fd44
Davi Nobre <davi.nbtec@gmail.com>
2025-11-07 11:01:42 -0300

    chore: organização de estrutura de pastas

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+<hr>
+<footer>
+    <p style="text-align: center;">&copy; 2025 Doctor Vet - Todos os direitos reservados.</p>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,9 @@
+    <header>
+        <nav>
+            <a href="index.php">Home</a> |
+            <a href="cadastro.php">Cadastro</a> |
+            <a href="login.php">Login</a> |
+            <a href="servicos.php">Serviços</a>
+        </nav>
+        <hr>
+    </header>

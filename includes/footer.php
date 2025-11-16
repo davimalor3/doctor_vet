81dee9c25b1a8186763f28026842118d236bb36e
Davi Nobre <davi.nbtec@gmail.com>
2025-11-16 01:14:19 -0300

    feat(includes): criação do footer

--- includes/footer.php
+++ includes/footer.php
@@ -2,3 +2,8 @@
 <footer>
     <p style="text-align: center;">&copy; 2025 Doctor Vet - Todos os direitos reservados.</p>
 </footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>

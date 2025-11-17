a08a0d2c159e14b480f794ec45ee1f70b0ecaa78
Davi Nobre <davi.nbtec@gmail.com>
2025-11-16 21:47:42 -0300

    feat(controllers): implementação da criação de agendamento

--- controllers/criar_agendamento.php
+++ controllers/criar_agendamento.php
@@ -13,10 +13,14 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
 
 $pet_id        = $_POST['pet_id'];
 $especialidade = $_POST['especialidade'];
-$data_hora     = $_POST['data_hora'];
+$data          = $_POST['data'];
+$hora          = $_POST['hora'];
+//converte para formato do banco de dados
+$data_hora = "$data $hora:00";
 $observacoes   = trim($_POST['observacoes'] ?? '');
 
-// valida se o pet pertence ao usuário
+
+// validar pet pertence ao usuário
 $pet = Pets::get($pet_id, $_SESSION['user_id']);
 if (!$pet) {
     $_SESSION['error'] = "Pet inválido.";
@@ -24,8 +28,30 @@ if (!$pet) {
     exit;
 }
 
-if (empty($pet_id) || empty($especialidade) || empty($data_hora)) {
-    $_SESSION['error'] = "Preencha os campos obrigatórios.";
+if (empty($pet_id) || empty($especialidade) || empty($data) || empty($hora)) {
+    $_SESSION['error'] = "Preencha todos os campos obrigatórios.";
+    header("Location: ../agendar.php");
+    exit;
+}
+
+// verifica se horário está ocupado
+if (Agendamento::horarioOcupado($data_hora)) {
+    $_SESSION['error'] = "Este horário já está agendado. Escolha outro.";
+    header("Location: ../agendar.php");
+    exit;
+}
+//verifica se o dia ou hora ja pasou
+if (strtotime($data_hora) < time()) {
+    $_SESSION['error'] = "A data/hora selecionada já passou.";
+    header("Location: ../agendar.php");
+    exit;
+}
+
+// valida horario de funcionamento
+$horaSelecionada = date("H:i", strtotime($data_hora));
+
+if ($horaSelecionada < "08:00" || $horaSelecionada > "18:00") {
+    $_SESSION['error'] = "O horário deve ser entre 08:00 e 18:00.";
     header("Location: ../agendar.php");
     exit;
 }

37edfb7944b4933e5b238141f182663b1d2b3432
Davi Nobre <davi.nbtec@gmail.com>
2025-11-16 21:47:42 -0300

    chore(includes): ajustes no footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,8 @@
 <hr>
 <footer>
-    <p style="text-align: center;">&copy; 2025 Doctor Vet - Todos os direitos reservados.</p>
+    <p style="text-align: center;">&copy; 2025 Doctor Vet - Todos os direitos reservados. | Desenvolvido por <a
+            href="https://github.com/davimalor3">AZXTech</a>.
+    </p>
 </footer>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>

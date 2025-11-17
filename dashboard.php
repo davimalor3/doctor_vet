5da3c7d7ccdd902775e48a76aa24e06143ad4647
Davi Nobre <davi.nbtec@gmail.com>
2025-11-16 21:47:43 -0300

    feat(root): adicionado botão de cadastrar pet

--- dashboard.php
+++ dashboard.php
@@ -1,66 +1,69 @@
 <?php
 require_once __DIR__ . "/config/session_config.php";
 require_once __DIR__ . "/config/auth.php";
-// middleware - verifica se está logado e redireciona para login
+// middleware
 require_login();
 include 'includes/header.php';
 ?>
 
 <style>
-    .dashboard-welcome {
-        background: #f8f9fa;
-        border-radius: 12px;
-        padding: 25px;
-    }
+.dashboard-welcome {
+    background: #f8f9fa;
+    border-radius: 12px;
+    padding: 25px;
+}
 
-    .action-card {
-        background: #fff;
-        border: 1px solid #e5e5e5;
-        border-radius: 12px;
-        padding: 25px;
-        transition: 0.25s ease;
-    }
+.action-card {
+    background: #fff;
+    border: 1px solid #e5e5e5;
+    border-radius: 12px;
+    padding: 25px;
+    transition: 0.25s ease;
+}
 
-    .action-card:hover {
-        transform: translateY(-6px);
-        box-shadow: 0 6px 18px rgba(0, 0, 0, 0.1);
-    }
+.action-card:hover {
+    transform: translateY(-6px);
+    box-shadow: 0 6px 18px rgba(0, 0, 0, 0.1);
+}
 
-    .logout-btn {
-        font-size: 1.1rem !important;
-        padding: 10px 24px !important;
-        border-radius: 10px;
-    }
+.logout-btn {
+    font-size: 1.1rem !important;
+    padding: 10px 24px !important;
+    border-radius: 10px;
+}
 
-    .btn-cadastrar-top {
-        font-size: 1.05rem;
-        padding: 10px 20px;
-        border-radius: 10px;
-        background-color: var(--primary);
-        border-color: var(--primary);
-        color: #fff;
-    }
+.btn-cadastrar-top {
+    font-size: 1.05rem;
+    padding: 10px 20px;
+    border-radius: 10px;
+    background-color: var(--primary);
+    border-color: var(--primary);
+    color: #fff;
+}
 
-    .btn-cadastrar-top:hover {
-        background-color: #178f74;
-        border-color: #178f74;
-    }
+.btn-cadastrar-top:hover {
+    background-color: #178f74;
+    border-color: #178f74;
+}
 
-    .menu-link-top {
-        font-size: 1.05rem;
-        font-weight: 600;
-        color: #2b3990 !important;
-        padding: 8px 14px !important;
-    }
+.menu-link-top {
+    font-size: 1.05rem;
+    font-weight: 600;
+    color: #2b3990 !important;
+    padding: 8px 14px !important;
+}
 </style>
 
-<!-- CONTEUDO PRINCIPAL-->
+<!-- CONTEÚDO PRINCIPAL-->
 <div class="container py-5">
 
     <!-- BEM-VINDO -->
     <div class="dashboard-welcome mb-5">
         <h3 class="fw-bold" style="color:#2b3990;">Bem-vindo(a), <?php echo $_SESSION['nome']; ?>!</h3>
         <p class="text-muted mb-0">Gerencie seus pets, agendamentos e histórico de forma simples.</p>
+        <h5 style="text-align: right;">Tem um animalzinho? Então, cadastre seu pet.<br><br>
+            <a href="cadastro_pet.php" class="btn btn-success">Cadastrar Pet</a>
+        </h5>
     </div>
 
     <!-- CARDS PRINCIPAIS -->

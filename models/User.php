c2c6f9311e1e1aaccda16cd3a2b789d0f936be12
Davi Nobre <davi.nbtec@gmail.com>
2025-11-20 11:12:25 -0300

    feat(useR): adicionado demais métodos de CRUD

--- models/User.php
+++ models/User.php
@@ -19,8 +19,7 @@ class User
     }
 
     // Criar novo usuário
-    public function create($nome, $email, $telefone, $senhaHash)
-    {
+    public function create($nome, $email, $telefone, $senhaHash){
         $stmt = $this->pdo->prepare("
             INSERT INTO users (nome, email, telefone, senha)
             VALUES (?, ?, ?, ?)
@@ -30,4 +29,39 @@ class User
 
         return $this->pdo->lastInsertId(); // aqui retorna o ID do novo usuário
     }
+
+    public function updateProfile($id, $nome, $email, $telefone){
+        $sql = "UPDATE users SET 
+            nome = :nome,
+            email = :email,
+            telefone = :telefone
+            WHERE id = :id";
+
+        $stmt = $this->pdo->prepare($sql);
+
+        return $stmt->execute([
+            'nome'      => $nome, 
+            'email'     => $email, 
+            'telefone'  => $telefone, 
+            'id'        => $id
+        ]);
+    }
+
+    public function updatePassword($id, $senhaHash){
+        $stmt = $this->pdo->prepare("
+            UPDATE users
+            SET senha = ?
+            WHERE id = ?
+        ");
+        return $stmt->execute([$senhaHash, $id]);
+    }
+
+    public function delete($id){
+        $stmt = $this->pdo->prepare("DELETE FROM users WHERE id = ?");
+        return $stmt->execute([$id]);
+    }
+    
 }
+
+
+
